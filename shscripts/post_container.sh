ee3b604ad067bf2b19c12bfcfa35649f04bcd104
Muthu Kamalan <muthukamalan98@gmail.com>
2025-02-22 10:00:05 +0530

    add unzip for Datasets

--- shscripts/post_container.sh
+++ shscripts/post_container.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 apt update
-apt install -y curl build-essential ca-certificates
+apt install -y curl build-essential ca-certificates unzip 
+# unzip and curl for "Downloading Datasets"
+# build-essential for "Make file"
