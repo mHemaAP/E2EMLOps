82e493ad2c4d15e3a81c985cde6c66f6cc887448
abhiyagupta <abhiya.gupta@gmail.com>
2025-05-11 00:43:11 +0530

    added my bucket name

--- src/backend/torch_local/transfer_mar.py
+++ src/backend/torch_local/transfer_mar.py
@@ -11,8 +11,6 @@ from dotenv import load_dotenv
 # Set environment variables for AWS
 load_dotenv() 
 
-# Get secrets from environment variables
-s3_bucket = os.getenv("S3_BUCKET_NAME")
 
 def upload_folder_to_s3(local_folder, bucket_name, s3_prefix):
     """
@@ -48,7 +46,7 @@ for project in all_projects:
     # Example uses a custom handler named 'custom_handler.py' in the current dir
     handler_path = f"src/backend/torchserve_app/{project}_handler.py"   # Update if needed
 
-    #s3_bucket = "mybucket-emlo-mumbai"
+    s3_bucket = "abhiya-mlops-project"
     s3_key_prefix = f"kserve-ig/{project}-classifier-dummy"  # e.g., "models/sports"
     # s3://mybucket-emlo-mumbai/kserve-ig/sports-classifier-dummy/
 
